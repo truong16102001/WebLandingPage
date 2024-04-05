72d8a4f99225f2aad49356516b1519a2ae67f844
NguyenDinhTruong <truongx62001@gmail.com>
2024-04-06 01:16:56 +0700

    second commit

---
+++ assets/js/app.js
@@ -0,0 +1,40 @@
+const navbar = document.querySelector(".navbar");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navLinksLi = document.querySelectorAll('.nav-links li')
+
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+navLinksLi.forEach(li => li.addEventListener('click', ()=>{
+    navLinksLi.forEach(li=>li.classList.remove('active'))
+    li.classList.add('active')
+
+    hamburger.classList.remove('active')
+    navLinks.classList.remove('active')
+
+}))
+
+// typed js
+var typedOptions = {
+    strings: ["Hi, I'm Chao", "Welcome!"],
+    typeSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    backDelay: 2000
+  };
+
+var typed = new Typed("#hero-titles", typedOptions);
+
+// AOS
+AOS.init();
